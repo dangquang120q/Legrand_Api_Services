bc4dcd535f8dd4ed6073b44daf69492afd2195e8
kien.nguyen3 <kien.nguyen3@ekoios.vn>
2024-05-29 09:06:44 +0700

    try

--- api/services/socket/data-utils.js
+++ api/services/socket/data-utils.js
@@ -1,8 +1,8 @@
 module.exports = {
     extractData: function (request) {
         const { data } = request;
-        const header = data.subString(0, 6);
-        const body = data.subString(6, data.length - 1);
+        const header = data.substring(0, 6);
+        const body = data.substring(6, data.length - 1);
         const end = data[data.length-1];
 
         return {header, body, end};

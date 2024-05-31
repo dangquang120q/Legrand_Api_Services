68fadb05b45dadb066d2019fdc7f77b78fb3c4ef
dangquang120q <dangquangpltnvn@gmail.com>
2024-06-01 01:13:49 +0700

    update

--- api/services/socket/checkPing.js
+++ api/services/socket/checkPing.js
@@ -13,7 +13,7 @@ module.exports = {
       let dataTime = await sails
         .getDatastore(process.env.MYSQL_DATASTORE)
         .sendNativeQuery(sqlTime);
-      if (Date.now() - dataTime["rows"][0] < 120000) {
+      if (Date.now() - dataTime["rows"][0]["last_ping_time"] < 120000) {
         result = 0;
       }
       else{

59e91a86933fb266c02c0655cfc16476e42edabb
kien.nguyen3 <kien.nguyen3@ekoios.vn>
2024-06-03 15:43:37 +0700

    test

--- api/services/socket/data-utils.js
+++ api/services/socket/data-utils.js
@@ -5,6 +5,10 @@ module.exports = {
         const header = request.substring(0, firstPosBody);
         const body = request.substring(firstPosBody, lastPosBody + 1);
         const end = request.substring(lastPosBody + 1, request.length);
+        for (i = 0; i < header.length; i++) {
+            var hex = header.charCodeAt(i).toString(16);
+            console.log("header char code " + i + " -- " + hex);
+        }
         console.log("header " + header.toString(16));
         console.log("end " + end.toString(16));
         return {header, body, end};

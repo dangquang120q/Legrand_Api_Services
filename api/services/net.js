a74fb0ad36fc1e09cf3dbf458c68df2cfd3dd30e
kien.nguyen3 <kien.nguyen3@ekoios.vn>
2024-06-03 18:14:25 +0700

    done

--- api/services/net.js
+++ api/services/net.js
@@ -88,10 +88,6 @@ server.on("secureConnection", function (socket) {
       }
       //echo data
       console.log("response-- " + header.concat(JSON.stringify(response)).concat(end));
-      for (i = 0; i < header.concat(JSON.stringify(response)).concat(end).length; i++) {
-        var hex = header.concat(JSON.stringify(response)).concat(end).charCodeAt(i).toString(16);
-        console.log("header response char code " + i + " -- " + hex);
-      }
       var is_kernel_buffer_full = socket.write(header.concat(JSON.stringify(response)).concat(end));
       setTimeout(async () => {
         let response = await checkPing(list_account[socket.remoteAddress]);

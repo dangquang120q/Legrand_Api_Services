094d3af9d513c8f688abc3fd78b188dd72600189
dangquang120q <dangquangpltnvn@gmail.com>
2024-06-08 22:37:06 +0700

    update

--- api/services/socket/data-report.js
+++ api/services/socket/data-report.js
@@ -1,46 +1,103 @@
+const sqlString = require("sqlstring");
+
 module.exports = {
-  addDevice: async function (request) {
-    const { data } = request;
-
-    const response = {
-      result: 0,
-    };
-    return response;
+  addDevice: async function (request, lts_mac) {
+    try {
+      const response = {
+        result: 0,
+      };
+      console.log("request == " + request);
+      console.log("lts_mac == " + lts_mac);
+      let result = 0;
+      response.packetNo = request.packetNo;
+      response.result = result;
+      return response;
+    } catch {
+      const response = {
+        result: -1,
+      };
+      response.packetNo = request.packetNo;
+      return response;
+    }
   },
 
-  delDevice: async function (request) {
-    const { data } = request;
-
-    const response = {
-      result: 0,
-    };
-    return response;
+  delDevice: async function (request, lts_mac) {
+    try {
+      const response = {
+        result: 0,
+      };
+      console.log("request == " + request);
+      console.log("lts_mac == " + lts_mac);
+      let result = 0;
+      response.packetNo = request.packetNo;
+      response.result = result;
+      return response;
+    } catch {
+      const response = {
+        result: -1,
+      };
+      response.packetNo = request.packetNo;
+      return response;
+    }
   },
 
-  switchDevice: async function (request) {
-    const { data } = request;
-
-    const response = {
-      result: 0,
-    };
-    return response;
+  switchDevice: async function (request, lts_mac) {
+    try {
+      const response = {
+        result: 0,
+      };
+      console.log("request == " + request);
+      console.log("lts_mac == " + lts_mac);
+      let result = 0;
+      response.packetNo = request.packetNo;
+      response.result = result;
+      return response;
+    } catch {
+      const response = {
+        result: -1,
+      };
+      response.packetNo = request.packetNo;
+      return response;
+    }
   },
 
-  battery: async function (request) {
-    const { data } = request;
-
-    const response = {
-      result: 0,
-    };
-    return response;
+  battery: async function (request, lts_mac) {
+    try {
+      const response = {
+        result: 0,
+      };
+      console.log("request == " + request);
+      console.log("lts_mac == " + lts_mac);
+      let result = 0;
+      response.packetNo = request.packetNo;
+      response.result = result;
+      return response;
+    } catch {
+      const response = {
+        result: -1,
+      };
+      response.packetNo = request.packetNo;
+      return response;
+    }
   },
 
-  alarm: async function (request) {
-    const { data } = request;
-
-    const response = {
-      result: 0,
-    };
-    return response;
+  alarm: async function (request, lts_mac) {
+    try {
+      const response = {
+        result: 0,
+      };
+      console.log("request == " + request);
+      console.log("lts_mac == " + lts_mac);
+      let result = 0;
+      response.packetNo = request.packetNo;
+      response.result = result;
+      return response;
+    } catch {
+      const response = {
+        result: -1,
+      };
+      response.packetNo = request.packetNo;
+      return response;
+    }
   },
 };

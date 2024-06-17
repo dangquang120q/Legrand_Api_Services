533be7a864ae6ea2b35ef95fae7106cb22cf2d0e
dangquang120q <dangquangpltnvn@gmail.com>
2024-06-17 17:41:44 +0700

    update

--- api/services/socket/update-lts.js
+++ api/services/socket/update-lts.js
@@ -1,10 +1,27 @@
 const sqlString = require("sqlstring");
 module.exports = {
-  firmWareInfo: async function (request) {
-    const { data } = request;
-
+  firmWareInfo: async function (request, lts_mac) {
+    console.log("request == " + JSON.stringify(request));
+    console.log("lts_mac == " + lts_mac);
     const response = {
       result: 0,
+    };
+    let sql = sqlString.format("Select * from lts_lastest_firmware_ver ORDER BY id DESC LIMIT 1;");
+    let data2 = await sails
+      .getDatastore(process.env.MYSQL_DATASTORE)
+      .sendNativeQuery(sql);
+    let result = 0;
+    response.packetNo = request.packetNo;
+    response.result = result;
+    response.data = {
+      system: data2["rows"][0]["system_img"],
+      gatewayDn: data2["rows"][0]["gatewayDn"],
+      account: data2["rows"][0]["account"],
+      password: data2["rows"][0]["password"],
+      accessUrl: data2["rows"][0]["access_url"],
+      port: data2["rows"][0]["port"],
+      ip: data2["rows"][0]["ip"],
+      describe: data2["rows"][0]["describe"]
     };
     return response;
   },

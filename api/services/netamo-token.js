940fa43b619a473ef401bd327169f6606085ed2e
nltquynh875 <nltquynh875@gmail.com>
2024-06-21 15:40:37 +0700

    fix

--- api/services/netamo-token.js
+++ api/services/netamo-token.js
@@ -18,14 +18,17 @@ module.exports = {
       } = params;
 
       log("Get netamo oauth token: " + JSON.stringify(params));
-      const res = await axiosNetamo.post("/oauth2/token", {
-        grant_type,
-        client_id,
-        client_secret,
-        code,
-        redirect_uri,
-        scope,
-      });
+      const res = axiosNetamo
+        .post("/oauth2/token", {
+          grant_type,
+          client_id,
+          client_secret,
+          code,
+          redirect_uri,
+          scope,
+        })
+        .then((res) => log(res))
+        .catch((err) => log(err));
       log("Netamo auth token:" + JSON.stringify(res));
       return res;
     } catch (error) {

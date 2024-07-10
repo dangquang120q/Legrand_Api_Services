1f8f6e47dd996c69818c3bc25610449ab661db59
dangquang120q <dangquangpltnvn@gmail.com>
2024-07-11 00:49:10 +0700

    update

--- api/services/socket/data-query.js
+++ api/services/socket/data-query.js
@@ -159,6 +159,7 @@ module.exports = {
         },
       });
       const dataWeatherPollution = await resPollution.json();
+      console.log("weather Pollution data == " + JSON.stringify(dataWeatherPollution));
 
       let listWeather = [];
       for (let index = 0; index < dataWeather["list"].length; index++) {

1d309f2ed2d8e3258bceb3f492e38192e6f607be
nltquynh875 <nltquynh875@gmail.com>
2024-07-10 17:20:30 +0700

    fix: mapHome

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -458,7 +458,7 @@ module.exports = {
         .getDatastore(process.env.MYSQL_DATASTORE)
         .sendNativeQuery(sql);
       const ref = data["rows"][1][0]["ref"];
-
+      console.log(data["rows"], ref);
       if (ref == 1) {
         response = new HttpResponse(
           { msg: "Map Home Successfull", homes: data["rows"][0] },

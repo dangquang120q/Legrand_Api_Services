c2237dd16fdf75b8f58601b4421f4dc547b29c99
qquynh217 <nltquynh875@gmail.com>
2024-07-11 22:00:28 +0700

    fix: checkJwtoken

--- api/policies/checkJwtoken.js
+++ api/policies/checkJwtoken.js
@@ -1,19 +1,28 @@
 const { HttpResponse } = require("../services/http-response");
-const sqlString = require('sqlstring');
-const jwtoken = require('../services/jwtoken');
-const {log} = require('../services/log');
+const sqlString = require("sqlstring");
+const jwtoken = require("../services/jwtoken");
+const { log } = require("../services/log");
 
 module.exports = async function (req, res, next) {
   const jwtToken = req.headers["auth-token"];
   try {
     let decodedToken = jwtoken.decode(jwtToken);
     let userId = decodedToken["userId"];
-    let sqlCheck = sqlString.format("Select id from user_account where user_id = ? and login_token = ?", [userId,jwtToken]);
-    let dataCheck = await sails.getDatastore(process.env.MYSQL_DATASTORE).sendNativeQuery(sqlCheck);
+    let sqlCheck = sqlString.format(
+      "Select id from user_account where user_id = ? and login_token = ?",
+      [userId, jwtToken]
+    );
+    let dataCheck = await sails
+      .getDatastore(process.env.MYSQL_DATASTORE)
+      .sendNativeQuery(sqlCheck);
     let response;
     if (dataCheck["rows"].length == 0) {
-        response = new HttpResponse({msg: "Invalid Token"}, { statusCode: 401, error: true });
-        return res.ok(response);
+      response = new HttpResponse(null, {
+        statusCode: 401,
+        error: true,
+        errorMsg: "Invalid Token",
+      });
+      return res.ok(response);
     }
     return next();
   } catch (error) {

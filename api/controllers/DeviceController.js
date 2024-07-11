08dafe286e3e0f98f3de91c0d09d9894ee3d2d95
qquynh217 <nltquynh875@gmail.com>
2024-07-12 00:41:37 +0700

    fix: turnOnLight

--- api/controllers/DeviceController.js
+++ api/controllers/DeviceController.js
@@ -18,7 +18,7 @@ module.exports = {
     try {
       const data = await setState({
         action: SET_STATE_ACTION.turnOnLight,
-        value: on,
+        value: on == "true" ? true : false,
         home_id: net_home_id,
         module_id: device_id,
         bridge: bridge,

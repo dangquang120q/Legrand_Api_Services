9ba5d8bcac7c47e77651a1bd955879fd14e49460
dangquang120202q <dangquangpltnvn@gmail.com>
2024-07-12 12:54:48 +0700

    update

--- api/services/socket/data-report.js
+++ api/services/socket/data-report.js
@@ -3,12 +3,19 @@ const sqlString = require("sqlstring");
 module.exports = {
   addDevice: async function (request, lts_mac) {
     try {
+      const { data } = request;
       const response = {
         result: 0,
       };
       console.log("request == " + request);
       console.log("lts_mac == " + lts_mac);
       let result = 0;
+      let sql = sqlString.format(
+        "update lts_device_control set lts_device_version = lts_device_version + ? where lts_mac = ?", [data["has"].length,lts_mac]
+      );
+      await sails
+        .getDatastore(process.env.MYSQL_DATASTORE)
+        .sendNativeQuery(sql);
       response.packetNo = request.packetNo;
       response.result = result;
       return response;

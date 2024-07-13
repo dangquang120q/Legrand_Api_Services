52ba12e8266ce88e6de45c375e3b3fd1037955bc
qquynh217 <nltquynh875@gmail.com>
2024-07-14 01:13:24 +0700

    fix: getRoomDetail add controlType

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -642,15 +642,16 @@ module.exports = {
         humidity: room["humidity"] || null,
         reachable: room["reachable"] || false,
         devices: {
-          lights: roomDevices
-            .filter((item) => DEVICE_CODES.lights.includes(item.type))
+          lights: roomDevices.filter((item) =>
+            DEVICE_CODES.lights.includes(item.type)
+          ),
+
+          curtains: roomDevices
+            .filter((item) => DEVICE_CODES.rollerShutter.includes(item.type))
             .map((item) => ({
               ...item,
               controlType: item["target_position:step"] >= 100 ? 0 : 1,
             })),
-          curtains: roomDevices.filter((item) =>
-            DEVICE_CODES.rollerShutter.includes(item.type)
-          ),
         },
       };
       response = new HttpResponse(response_data, {

b5397c01cddf9cf3b9b2765d26163951af56d1a1
qquynh217 <nltquynh875@gmail.com>
2024-07-15 20:58:43 +0700

    fix: getRoomDetail reachable

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -627,11 +627,11 @@ module.exports = {
           ?.filter((item) => item.room_id == room_id)
           .map((item) => ({
             ...item,
-            reachable:
-              errors?.code == 6 &&
-              (errors?.id == item["bridge"] || errors?.id == item["id"])
+            reachable: errors?.find((error) =>
+              item["id"] == error.id || item["bridge"] == error.id
                 ? false
-                : true,
+                : true
+            ),
           })) || [];
       roomDevices = roomDevices.map((item) => {
         const device = homeStatus?.modules?.find(

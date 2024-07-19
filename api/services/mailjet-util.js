127163e25e55610126da0d13f5df9a95d616125f
kien.nguyen3 <kien.nguyen3@ekoios.vn>
2024-07-19 21:15:07 +0700

    abc

--- api/services/mailjet-util.js
+++ api/services/mailjet-util.js
@@ -3,28 +3,24 @@ const log = require('./log').log;
 
 
 module.exports = {
-    sendOTPEmail: async function (to, subject, text, otp) {
+    sendOTPEmail: async function (to, text, otp) {
         const mailjet = Mailjet.apiConnect(
             process.env.MAILJET_API_KEY,
             process.env.MAILJET_SECRET_KEY
         );
-        var html = '<div style="font-family: Helvetica, Arial, sans - serif; min - width: 1000px; overflow: auto; line - height: 2">'
+        var html = '<div style="font-family: Helvetica, Arial, sans-serif; min-width: 1000px; overflow: auto; line-height: 2">'
             + '< div style = "margin:50px auto;width:70%;padding:20px 0" >'
-            + '<div style="border-bottom:1px solid #eee">'
-            + '<a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Your Brand</a>'
-            + '</div>'
             + '<p style="font-size:1.1em">Hi,</p>'
-            + '<p>Thank you for choosing Your Brand. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p>'
+            + '<p>Use the following OTP to complete your Forgot Password procedures. OTP is valid for 2 days</p>'
             + '<h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">' + otp +'</h2>'
-            + '<p style="font-size:0.9em;">Regards,<br />Your Brand</p>'
             + '<hr style="border:none;border-top:1px solid #eee" />'
-            + '<div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">'
-            + '<p>Your Brand Inc</p>'
-            + '<p>1600 Amphitheatre Parkway</p>'
-            + '<p>California</p>'
+            // + '<div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">'
+            // + '<p>Your Brand Inc</p>'
+            // + '<p>1600 Amphitheatre Parkway</p>'
+            // + '<p>California</p>'
+            // + '</div>'
             + '</div>'
-            + '</div >'
-            + '</div >'
+            + '</div>'
         log("TO: " + to);
         const request = mailjet
             .post('send', { version: 'v3.1' })
@@ -41,7 +37,7 @@ module.exports = {
                                 Name: "USER"
                             }
                         ],
-                        Subject: subject,
+                        Subject: "Forgot Password OTP",
                         TextPart: text,
                         HTMLPart: html
                     }

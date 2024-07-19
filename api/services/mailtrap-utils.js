dddd1203870f257b8f3089d07284784b3594290d
kien.nguyen3 <kien.nguyen3@ekoios.vn>
2024-07-19 22:15:46 +0700

    again

--- api/services/mailtrap-utils.js
+++ api/services/mailtrap-utils.js
@@ -4,8 +4,8 @@ const transporter = nodemailer.createTransport({
     host: 'smtp.mailtrap.io',
     port: 2525,
     auth: {
-        user: 'kiennt.k54@gmail.com', // Replace with your Mailtrap username
-        pass: 'Mrneo1991@!'  // Replace with your Mailtrap password
+        user: '0c919c9033104c', // Replace with your Mailtrap username
+        pass: 'df355faa4303e5'  // Replace with your Mailtrap password
     }
 });
 

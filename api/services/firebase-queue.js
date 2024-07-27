028e88bc41f12ec2be596602c6c07a871bab77ad
kien.nguyen3 <kien.nguyen3@ekoios.vn>
2024-07-28 00:17:40 +0700

    abc

--- api/services/firebase-queue.js
+++ api/services/firebase-queue.js
@@ -1,7 +1,7 @@
 const Queue = require('bull');
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./config/legrand-dev-api-firebase-adminsdk-r0fjc-901a1347a2.json');//'./config/legrand-dev-api-firebase-adminsdk-r0fjc-901a1347a2.json');
+const serviceAccount = require('../../config/legrand-dev-api-firebase-adminsdk-r0fjc-901a1347a2.json');//'./config/legrand-dev-api-firebase-adminsdk-r0fjc-901a1347a2.json');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });

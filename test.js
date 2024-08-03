b6a13c1d2011b387a58ad568a1e925a1aa6008bb
kien.nguyen3 <kien.nguyen3@ekoios.vn>
2024-08-04 00:57:10 +0700

    try

---
+++ test.js
@@ -0,0 +1,16 @@
+const crypto = require('crypto');
+
+// Dữ liệu cần mã hóa
+const data = 'LEGRAND_SC#BA671EFEFFB96F8C#1#0';
+// Khóa AES-128-ECB
+const key = 'E26DC731BF67F664D28E90E008B083A6';
+
+// Tạo một đối tượng cipher
+const cipher = crypto.createCipheriv('aes-128-ecb', Buffer.from(key, 'hex'), null);
+cipher.setAutoPadding(true);
+
+// Mã hóa dữ liệu
+let encrypted = cipher.update(data, 'utf8', 'base64');
+encrypted += cipher.final('base64');
+
+console.log('Dữ liệu đã mã hóa:', encrypted);

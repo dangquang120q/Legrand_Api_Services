bcdc51aed6ba1c6e8eaaedde947e0bc7c594a8a9
dangquang120q <dangquangpltnvn@gmail.com>
2024-08-10 08:49:40 +0700

    update

--- api/services/socket/data-report.js
+++ api/services/socket/data-report.js
@@ -124,11 +124,25 @@ module.exports = {
 
   alarm: async function (request, lts_mac) {
     try {
+      const { data } = request;
       const response = {
         result: 0,
       };
       console.log("request == " + request);
       console.log("lts_mac == " + lts_mac);
+      let sql = sqlString.format(
+        "update lts_device_detail set alampStatus = ? where lts_mac = ? and deviceId = ?", [data.alarmType,data.gatewayDn,data.deviceId]
+      );
+      await sails
+        .getDatastore(process.env.MYSQL_DATASTORE)
+        .sendNativeQuery(sql);
+      let insertSql = sqlString.format(
+        "insert into sensor_alarm_history(deviceId, updated_by) values (?, ?)",
+        [data.gatewayDn, data.alarmType]
+      );
+      await sails
+        .getDatastore(process.env.MYSQL_DATASTORE)
+        .sendNativeQuery(insertSql);
       let result = 0;
       response.packetNo = request.packetNo;
       response.result = result;

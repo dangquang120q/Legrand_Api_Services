a72cfcaac54f2c9349e7e2fd6b524f73f236fe95
qquynh217 <nltquynh875@gmail.com>
2024-08-16 23:07:03 +0700

    fix: getListHomeNetatmo

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -765,7 +765,7 @@ module.exports = {
             const airConditioner = roomDevices.find((item) =>
               DEVICE_CODES.airConditioner.includes(item.type)
             );
-
+            log("devices: " + JSON.stringify(roomDevices));
             // Push room to array
             rooms.push({
               ...room,
@@ -775,7 +775,7 @@ module.exports = {
               lightStatus: lights ? "ON" : "OFF",
               airConditionerStatus: airConditioner
                 ? null
-                : airConditioner.mode == "max"
+                : room["cooling_setpoint_mode"] == "max"
                 ? "ON"
                 : "OFF",
             });

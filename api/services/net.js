e6dc06120303259b6b6ef326f5f9181cf08ae052
kien.nguyen3 <kien.nguyen3@ekoios.vn>
2024-08-20 18:17:00 +0700

    a

--- api/services/net.js
+++ api/services/net.js
@@ -289,6 +289,7 @@ const upgradeVersion = async (request) => {
 const controlLight = async (request) => {
   try {
     Object.values(list_account_test).forEach(async (account) => {
+      
       // Lấy socket của client từ account (giả sử list_account lưu trữ socket trực tiếp)
       let socket = account.socket; // Sửa lại tên biến socket nếu cần thiết
       if (socket) {

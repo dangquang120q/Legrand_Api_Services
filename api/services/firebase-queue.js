9cf04bde20f8aa9dcbd7b0b295aee5023e690095
dangquang120q <dangquangpltnvn@gmail.com>
2024-09-16 22:32:01 +0700

    update

--- api/services/firebase-queue.js
+++ api/services/firebase-queue.js
@@ -26,7 +26,7 @@ notificationQueue.process(async (job, done) => {
             notification: message.title,
             data: message.body
         });
-        console.log(JSON.stringify(response[0].error));
+        console.log(JSON.stringify(response.responses[0].error));
         console.log('Successfully sent message:', response);
         done(null, response);
     } catch (error) {

fcf436aa9caabd0c50c400c1256c70e4698c3281
dangquang120q <dangquangpltnvn@gmail.com>
2024-09-24 16:50:07 +0700

    update

--- api/services/socket/data-report.js
+++ api/services/socket/data-report.js
@@ -242,8 +242,7 @@ module.exports = {
 
       const message = {
         title: "Water Leakage",
-        body: "Water Leakage Detect - " + dataLocation["rows"][0]["location"],
-        data: {},
+        body: "Water Leakage Detect - " + dataLocation["rows"][0]["location"]
       }
       // Chia thành các batch nhỏ để tránh quá tải
       const batchSize = 500;

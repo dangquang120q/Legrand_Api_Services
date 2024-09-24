1f2fbe277cf5b080111804c6646c23292217f771
dangquang120q <dangquangpltnvn@gmail.com>
2024-09-24 16:53:27 +0700

    update

--- api/services/socket/data-report.js
+++ api/services/socket/data-report.js
@@ -114,8 +114,8 @@ module.exports = {
       const registrationTokens = dataFb.rows.map((device) => device.device_token);
 
       const message = {
-          title: "Switch Device",
-          body: "Switch Device - " + data.deviceId
+          switch: data.switch + "",
+          deviceId: data.deviceId + ""
       };
 
       // Chia thành các batch nhỏ để tránh quá tải

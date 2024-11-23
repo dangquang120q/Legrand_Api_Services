74a71b53d44b2e226caa6d581396555ad6bb765f
dangquang120q <dangquangpltnvn@gmail.com>
2024-11-23 10:50:42 +0700

    update

--- api/services/net.js
+++ api/services/net.js
@@ -507,7 +507,7 @@ const appDelDevice = async (request) => {
       // Lấy socket của client từ account (giả sử list_account lưu trữ socket trực tiếp)
       let socket = account.socket; // Sửa lại tên biến socket nếu cần thiết
       if (socket) {
-        if (account.dn == request.data["gatewayDn"]) {
+        if (account.dn == request.data["has"][0]["gatewayDn"]) {
           let { req, result } = await doAppDelDevice(request);
           let header = dataUtils
             .fromCharCodeData(68)

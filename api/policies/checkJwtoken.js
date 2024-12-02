f6ccb50f21e62db3b2bad7ab8572517767a06144
kien.nguyen3 <kien.nguyen3@ekoios.vn>
2024-12-02 13:10:12 +0700

    remove token

--- api/policies/checkJwtoken.js
+++ api/policies/checkJwtoken.js
@@ -18,8 +18,8 @@ module.exports = async function (req, res, next) {
     }
     let userId = decodedToken["userId"] || 0;
     let sqlCheck = sqlString.format(
-      "Select id, netatmo_access_token from user_account where user_id = ? and login_token = ?",
-      [userId, jwtToken]
+      "Select id, netatmo_access_token from user_account where user_id = ?",// and login_token = ?",
+      [userId]//, jwtToken]
     );
     let dataCheck = await sails
       .getDatastore(process.env.MYSQL_DATASTORE)

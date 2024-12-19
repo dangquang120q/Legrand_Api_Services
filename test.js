0a7a3de74897292d09c0855b101d8a6e85e606ea
kien.nguyen3 <kien.nguyen3@ekoios.vn>
2024-12-19 09:30:50 +0700

    a

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const CryptoJS = require("crypto-js");
 
 // Dữ liệu cần mã hóa
 // const data = "LEGRAND_SC#BA671EFEFFB96F8C#1#0";
-const data = "LEGRAND_SC#BA7C5B4E3F2D9C8E#1#0";
+// const data = "LEGRAND_SC#BA7C5B4E3F2D9C8E#1#0";
 // Khóa AES-128-ECB
 const key = "E26DC731BF67F664D28E90E008B083A6";
 const cipherText = "0ddWMQXtZXpARAEJrrl1hjuTbk1Tj7/MlwVhXEC7/Qo=";
@@ -38,18 +38,27 @@ const cipherText = "0ddWMQXtZXpARAEJrrl1hjuTbk1Tj7/MlwVhXEC7/Qo=";
 // console.log("Dữ liệu đã mã hóa:", encrypted);
 // console.log("Dữ liệu giải mã:", decrypted);
 
-let text = CryptoJS.enc.Base64.parse(cipherText);
-let keyHex = CryptoJS.enc.Hex.parse(key);
-let decrypted = CryptoJS.AES.decrypt(
-  cipherText,
-  keyHex,
-  {
-    mode: CryptoJS.mode.ECB,
-    padding: CryptoJS.pad.Pkcs7,
-  }
-);
+// let text = CryptoJS.enc.Base64.parse(cipherText);
+// let keyHex = CryptoJS.enc.Hex.parse(key);
+// let decrypted = CryptoJS.AES.decrypt(
+//   cipherText,
+//   keyHex,
+//   {
+//     mode: CryptoJS.mode.ECB,
+//     padding: CryptoJS.pad.Pkcs7,
+//   }
+// );
 
-// Convert the decrypted data back to a string
-let decryptedText = decrypted.toString(CryptoJS.enc.Utf8);
+// // Convert the decrypted data back to a string
+// let decryptedText = decrypted.toString(CryptoJS.enc.Utf8);
 
-console.log("Decrypted Text:", decryptedText);
+var data = [
+  'M01.L4A.16.09@gmail.com',
+  'M01.L4A.06.07@gmail.com'
+]
+;
+data.forEach(element => {
+  console.log(CryptoJS.MD5(element).toString());
+});
+
+// console.log("Decrypted Text:", decryptedText);

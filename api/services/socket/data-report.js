f5d872ae68a34bc0a76a38e8f59700a98a984fbe
dangquang120q <dangquangpltnvn@gmail.com>
2024-12-20 14:03:01 +0700

    update

--- api/services/socket/data-report.js
+++ api/services/socket/data-report.js
@@ -5,7 +5,7 @@ const notificationQueue = require("../firebase-queue");
 module.exports = {
   addDevice: async function (request, lts_mac) {
     try {
-      console.log("addDevice");
+      console.log("addDevice====" + JSON.stringify(request));
       const { data } = request;
       const response = {
         result: 0,
